18adfa0675e5ea2f0b9c19e21f1a93ff482ca585
amir-gz <amirgz98@gmail.com>
2024-02-26 12:07:29 +0330

    Added all files.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ["iranyekanBakh", "sans-serif"],
+      display: ["iranyekanBakh", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
