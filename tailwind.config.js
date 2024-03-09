9624e266c2326fbd10d2ea6ad93dfb039b276a3b
amir-gz <amirgz98@gmail.com>
2024-03-09 19:57:24 +0330

    search resualt page ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./sign-in.html"],
+  content: ["./*.html"],
   theme: {
     extend: {},
     fontFamily: {
